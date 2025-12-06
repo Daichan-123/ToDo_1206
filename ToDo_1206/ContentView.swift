950698578a608493bde30561e2c0c64731c566e6
Daiki Shimizu <mildlight28@gmail.com>
2025-12-06 11:39:04 +0900

    Initial Commit

---
+++ ToDo_1206/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDo_1206
+//
+//  Created by 清水大喜 on 2025/12/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDo_1206/ToDo_1206App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_1206App.swift
+//  ToDo_1206
+//
+//  Created by 清水大喜 on 2025/12/06.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_1206App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
